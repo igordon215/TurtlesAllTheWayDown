6acc863390d37f905e9bfe947857969eb4f8d9b8
igordon215 <igordon215@gmail.com>
2024-08-08 22:41:54 -0400

    getGCDTest

--- src/test/java/GCDTest.java
+++ src/test/java/GCDTest.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GCDTest {
@@ -20,7 +23,6 @@ class GCDTest {
 
     @Test
     public void TestGetGCD2() {
-
         //Given
         Long x2 = 12L;
         Long y2 = 10L;
@@ -30,8 +32,34 @@ class GCDTest {
         //Then
         System.out.println("GCD of " + x2 + " and " + y2 + " is: " + GCD.getGCD2(x2, y2));
         Assert.assertEquals(expected,actual);
+    }
 
+    @Test
+    public void getGCDTest() {
+        long p = 1000000L;
+        long q = 500000L;
 
+        // Time getGCD1
+        long startTime1 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            GCD.getGCD1(p, q);
+        }
+        long endTime1 = System.nanoTime();
+        long duration1 = endTime1 - startTime1;
+
+        // Time getGCD2
+        long startTime2 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            GCD.getGCD2(p, q);
+        }
+        long endTime2 = System.nanoTime();
+        long duration2 = endTime2 - startTime2;
+
+        // Compare durations
+        System.out.println("Time for getGCD1: " + duration1 + " ns");
+        System.out.println("Time for getGCD2: " + duration2 + " ns");
+
+        Assert.assertTrue("getGCD2 should be slower than getGCD1", duration2 > duration1);
     }
 
 
