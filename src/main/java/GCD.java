13ec2a7b80003b7e4967b88a2969e42461c6ee5e
igordon215 <igordon215@gmail.com>
2024-08-08 22:28:55 -0400

    gcd2

--- src/main/java/GCD.java
+++ src/main/java/GCD.java
@@ -1,11 +1,30 @@
 public class GCD {
 
-    public static Long getGCD(Long x, Long y) {
+    public static Long getGCD1(Long x, Long y) {
 
         if (y == 0) {
             return x;
         }
-        return getGCD(y, x % y);
+        return getGCD1(y, x % y);
 
     }
+
+    public Long getGCD2(Long x, Long y) {
+
+        Long counter = 0L;
+        if (y == 0) {
+            return x;
+        }
+
+        for (int i = 1; i <= x; i++) {
+
+            if ((x % i == 0) && (y % i == 0)) {
+
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
 }
