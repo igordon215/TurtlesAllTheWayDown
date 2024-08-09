8d5fe960e4bb0c6c75e959ce60a077ba06803e1c
igordon215 <igordon215@gmail.com>
2024-08-08 21:54:18 -0400

    testFactorial2

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -8,14 +8,27 @@ public class FactorialTest {
     @Test
     public void testFactorialOfZero() {
         // Given
-        int input = 0;
+        int input1 = 0;
 
         // When
-        Long result = Factorial.Factorial(input);
+        Long result1 = Factorial.Factorial(input1);
 
         // Then
-        System.out.println("Factorial of " + input + " is: " + result);
-        assertEquals(Long.valueOf(1), result);
+        System.out.println("Factorial of " + input1 + " is: " + result1);
+        assertEquals(Long.valueOf(1), result1);
+    }
+
+    @Test
+    public void testFactorialOfPositiveNumber() {
+        // Given
+        int input2 = 5;
+
+        // When
+        Long result1 = Factorial.Factorial(input2);
+
+        // Then
+        System.out.println("Factorial of " + input2 + " is: " + result1);
+        assertEquals(Long.valueOf(120), result1);
     }
 
 
