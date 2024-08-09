efc169503a342d72527b6be557b3e8c50d7edb65
igordon215 <igordon215@gmail.com>
2024-08-08 21:51:30 -0400

    testFactorial1

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,5 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FactorialTest {
+public class FactorialTest {
+
+    @Test
+    public void testFactorialOfZero() {
+        // Given
+        int input = 0;
+
+        // When
+        Long result = Factorial.Factorial(input);
+
+        // Then
+        System.out.println("Factorial of " + input + " is: " + result);
+        assertEquals(Long.valueOf(1), result);
+    }
+
 
 }
